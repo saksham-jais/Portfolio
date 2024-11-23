6b3c5f140c0c17ff9fdc1614e89a391b5c3c15a9
Saksham Jaiswal <116110942+saksham-jais@users.noreply.github.com>
2024-11-23 21:58:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+    const fadeInElement = document.querySelector('.img');
+  
+    fadeInElement.classList.add('visible'); // Trigger the fade-in transition
+
+    // const UpElement = document.querySelector('#Imglg');
+    // UpElement.classList.add('vsl'); // Trigger the fade-in transition
+  
+  };
